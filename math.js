9b7763fda92d5a49968387fbcf3f6c1dd2684743
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-28 22:12:59 +0100

    changed else if to if

--- math.js
+++ math.js
@@ -1,8 +1,7 @@
 function isPrime(num) {return false;
 
-  if (num < 1) {
-    return false;
-  }   else if(num%1===0 && num%2!=0 && num%3!=0
+  if (num < 1) {return false}  
+  if(num%1===0 && num%2!=0 && num%3!=0
 && num%5!=0) return true;
   }
 module.exports = isPrime;
