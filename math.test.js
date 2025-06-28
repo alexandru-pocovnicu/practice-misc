d856349eb53dddfd7229c54d443776e6fa8c4cea
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-28 22:40:25 +0100

    returns false if it divides by 2

--- math.test.js
+++ math.test.js
@@ -6,3 +6,6 @@ expect(isPrime(0)).toBe(false)
 it("returns true for prime numbers",()=>{
     expect(isPrime(7)).toEqual(true);
 })
+it("returns false if it divides by 2",()=>{
+    expect(isPrime(20)).toEqual(false);
+})
