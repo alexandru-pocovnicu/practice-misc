107b2000061a303e7ac92241683c2a3f4d13f1da
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-28 22:24:15 +0100

    changed to.Be to to.Equal

--- math.test.js
+++ math.test.js
@@ -4,5 +4,5 @@ it("returns false if a number is smaller than 1",()=>{
 expect(isPrime(0)).toBe(false)
 })
 it("returns true for prime numbers",()=>{
-    expect(isPrime(7)).toBe(true);
+    expect(isPrime(7)).toEqual(true);
 })
