ae8680c46d7f63dc8c9d725c506699e7fcddc339
alexandru-pocovnicu <109530683+alexandru-pocovnicu@users.noreply.github.com>
2025-06-28 22:42:01 +0100

    it returns false for 3,5,7

--- math.test.js
+++ math.test.js
@@ -9,3 +9,12 @@ it("returns true for prime numbers",()=>{
 it("returns false if it divides by 2",()=>{
     expect(isPrime(20)).toEqual(false);
 })
+it("returns false if it divides by ", () => {
+  expect(isPrime(30)).toEqual(false);
+});
+it("returns false if it divides by 5", () => {
+  expect(isPrime(25)).toEqual(false);
+});
+it("returns false if it divides by 7", () => {
+  expect(isPrime(21)).toEqual(false);
+});
